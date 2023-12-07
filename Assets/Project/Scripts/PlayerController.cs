358a89f8d818312325181cd885220da882420b1b
The Last Cookie <74693705+TheLastCockie@users.noreply.github.com>
2023-12-07 11:16:54 +0600

    Play scripting started

--- Assets/Project/Scripts/PlayerController.cs
+++ Assets/Project/Scripts/PlayerController.cs
@@ -7,20 +7,29 @@ public class PlayerController : MonoBehaviour
 {
     private CharacterController _characterController;
     private Vector3 _input;
-    private float _playerSpeed = 30;
-    private float horizontalInput;
-    private float verticalInput;
+    private float _playerSpeed = 10f;
+    private float _playerRotSpeed = 10f;
+    private float _horizontalInput;
+    private float _verticalInput;
+    private float _mouseXInput;
+    private float _mouseYInput;
+
     void Start()
     {
-        _characterController = gameObject.AddComponent<CharacterController>();
+        /*_characterController = gameObject.AddComponent<CharacterController>();*/
+        _characterController = GetComponent<CharacterController>();
     }
 
     void Update()
     {
-        horizontalInput = Input.GetAxis("Horizontal") * Time.deltaTime * _playerSpeed;
-        verticalInput = Input.GetAxis("vertical") * Time.deltaTime * _playerSpeed;
-        _input.x = horizontalInput;
-        _input.y = verticalInput;
+        _horizontalInput = Input.GetAxis("Horizontal") * Time.deltaTime * _playerSpeed;
+        _verticalInput = Input.GetAxis("Vertical") * Time.deltaTime * _playerSpeed;
+        _mouseXInput = Input.GetAxis("MouseX") * Time.deltaTime * _playerRotSpeed;
+        _mouseYInput = Input.GetAxis("Mousey") * Time.deltaTime * _playerRotSpeed;
+
+        transform.rotation =  Quaternion.Euler(_horizontalInput, _verticalInput, 0);
+        _input.x = _horizontalInput;
+        _input.z = _verticalInput;
         _characterController.Move(_input);
     }
 }
